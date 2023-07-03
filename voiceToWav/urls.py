bf6424741a55039aa8e30e3787841690cf487622
KaKino29 <83937183+KaKino29@users.noreply.github.com>
2023-07-03 15:12:15 +0900

    csrf token 적용

--- voiceToWav/urls.py
+++ voiceToWav/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
-    path('', views.main, name='main' ),
+    path('', views.main, name='main'),
     path('index',  views.mainProcess, name='mainProcess'),
-    
+
 
 ]
